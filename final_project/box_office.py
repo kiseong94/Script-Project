d85ac63b9fe30cfde59d20a50e4994f57c37298b
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2019-05-21 23:36:48 +0900

    박스오피스 순위 가져오기 GetBoxOfficeRankInfo구현
    박스오피스 순위대로 영화 가져오기 구현

---
+++ final_project/box_office.py
@@ -0,0 +1,36 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+
+
+def GetBoxOfficeRankInfo():
+
+    BoxOfficeRankInfo = []
+
+    URL =  "https://search.naver.com/search.naver?sm=top_hty&fbm=0&ie=utf8&query=%EB%B0%95%EC%8A%A4%EC%98%A4%ED%94%BC%EC%8A%A4"
+    req = urllib.request.Request(URL)
+    data = urllib.request.urlopen(req).read()
+
+    bs = BeautifulSoup(data, 'html.parser')
+
+    pages = bs.find_all("div", {"class": "_content"})
+
+    rank = 1
+    for page in pages:
+        page_list = []
+        l1 = page.find_all("strong")
+        l2 = page.find_all("img")
+        for i in range(len(l1)):
+            name = re.search('\>(.*?)\<', str(l1[i]))
+            img_url = re.search('src="(.*?)"', str(l2[i]))
+            info = dict( rank = rank ,name = name.group(1), img = img_url.group(1))
+            page_list.append(info)
+            rank+=1
+        BoxOfficeRankInfo.append(page_list)
+
+    for i in BoxOfficeRankInfo:
+        print(i)
+
+
+
+GetBoxOfficeRankInfo()
--- final_project/theater_info.py
+++ final_project/theater_info.py
@@ -1,8 +1,27 @@
+from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
-import MovieInfo
 import json
 import re
+
+
+CGVTheaterTable = None
+with open('CGVtheaterCode.json') as file:
+    CGVTheaterTable = json.load(file)
+#    print(CGVTheaterTable)
+#     for i in CGVTheaterTable:
+#         for j in i["AreaTheaterDetailList"]:
+#             name = j["TheaterName"]
+#             name = name.replace(" ","",4)
+#             if name == 'CINEdeCHEF센텀':
+#                 name = '씨네드쉐프센텀시티'
+#             name = name.replace('CINEdeCHEF','씨네드쉐프')
+#             t = (j["TheaterCode"],j["RegionCode"])
+#             theater_list[name] = t
+#
+# with open('CGVtheaterCode.json', 'w', encoding="utf-8") as make_file:
+#     json.dump(theater_list, make_file)
+
 
 
 location_table = ["서울특별시", "인천광역시", "경기도", "세종특별자치시","대전광역시","충청도","강원도","광주광역시","전라도","대구광역시","울산광역시","부산광역시","경상도","제주특별자치도"]
@@ -57,10 +76,86 @@ def getTheaterInfo(location, sub_location):
         theater_inform.append(d)
     return theater_inform
 
+def getMovieInfoFromNaver(theatercode):
+    movie_inform_list = []
+
+    baseURL = "http://movie.naver.com/movie/bi/ti/running.nhn?code="
+    URL =  baseURL + str(theatercode)
+    req = urllib.request.Request(URL)
+    data = urllib.request.urlopen(req).read()
+
+    bs = BeautifulSoup(data, 'html.parser')
+    #print(bs)
+    l1 = bs.find_all('th')
+    l2 = bs.find_all('td')
+
+    movie_name_list = []
+    time_list = []
+
+    for i in l1:
+        e = i.find('a')
+        name = re.search('\>(.*?)\<', str(e))
+
+        if name:
+            movie_name_list.append(name.group(1))
+
+    for i in l2:
+        time = re.findall('\d\d\:\d\d', str(i))
+        if time:
+            time_list.append(time)
+
+    for i in range(len(movie_name_list)):
+        movie_inform_list.append((movie_name_list[i],time_list[i]))
+
+    return movie_inform_list
+
+
+def getMovieInfoFromCGV(theater):
+    global CGVTheaterTable
+
+    movie_inform_list = []
+
+    theater_name = theater.replace(" ","")
+    baseURL = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=key1&theatercode=key0&date="
+    URL = baseURL.replace('key0', CGVTheaterTable[theater_name][0])
+    URL = URL.replace('key1', CGVTheaterTable[theater_name][1])
+
+    req = urllib.request.Request(URL)
+    data = urllib.request.urlopen(req).read()
+
+    bs = BeautifulSoup(data, 'html.parser')
+
+    #print(bs)
+
+    movie_name_list = []
+
+    l1 = bs.find("div", {"class" :"sect-showtimes"}).find_all("strong")
+    l2 = bs.find("div", {"class": "sect-showtimes"}).find_all("div",{"class": "col-times"})
+    time_list = []
+    for i in l2:
+        l = i.find_all("em")
+        time = re.findall('\d\d\:\d\d', str(l))
+        time_list.append(time)
+
+    for i in l1:
+        name = re.search('\s+(.*)\<', str(i))
+        if name:
+            movie_name_list.append(name.group(1))
+
+    for i in range(len(movie_name_list)):
+        movie_inform_list.append((movie_name_list[i],time_list[i]))
+
+    return movie_inform_list
+
+
+
+
+
+
 for t in getTheaterInfo(0,0):
     print(t['name'])
 
     if "씨네드쉐프" in t['name'] or "CGV" in t['name']:
-        print(MovieInfo.getMovieInfoFromCGV(t['name']))
+        print(getMovieInfoFromCGV(t['name']))
     else:
-        print(MovieInfo.getMovieInfoFromNaver(t['code']))
+        print(getMovieInfoFromNaver(t['code']))
